1d5f509ea388546b1f2c69c3243ef87db2834b3a
Adrian Wälchli <adrian@solarflare.lan>
2021-01-04 09:47:26 +0100

    modules for fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,4 +1,10 @@
 # use bash aliases
 source ~/.bash_aliases
 
+# new aliases
 alias reload='source ~/.config/fish/config.fish'
+
+# init shell for environment modules
+if test -e /etc/profile.d/modules.fish
+    source /etc/profile.d/modules.fish
+end
