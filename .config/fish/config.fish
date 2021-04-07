7d3d8a94e4d564e90db33d8876dca44eac68e62c
Adrian Wälchli <aedu.waelchli@gmail.com>
2021-04-07 23:56:56 +0200

    update conda init for fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -18,7 +18,8 @@ end
 set -x INF_PASSWORD_STORE_DIR ~/repositories/inf-pass      
 alias inf-pass='PASSWORD_STORE_DIR="$INF_PASSWORD_STORE_DIR" pass'
 
-# >>> conda initialize >>>
-# !! Contents within this block are managed by 'conda init' !!
-eval /usr/local/anaconda3/bin/conda "shell.fish" "hook" $argv | source
-# <<< conda initialize <<<
+if test -d /usr/local/anaconda3/
+    eval /usr/local/anaconda3/bin/conda "shell.fish" "hook" $argv | source
+else if test -d $HOME/anaconda3 
+    eval $HOME/anaconda3/bin/conda "shell.fish" "hook" $argv | source
+end
